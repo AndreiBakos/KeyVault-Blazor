81d1627563ad4edb97ac69e15171c58bfa1a67fa
Andrei Bakos <49485875+AndreiBakos@users.noreply.github.com>
2023-03-06 23:10:50 +0200

    Implement backend functionality (#2)
    * WIP + Added more backend functionality
    
    Thinks added to this commit:
    
    1) Created a few classes in order to help with retrieving data from controller and browser local storage
    1) Updated route page navigations to be responsive to local storage and token
    1) Added a few backend functionalities for mysecrets component
    1) Finished login page with backend functionality
    
    * WIP + Implemented backend on groups, signup and groups page
    
    * WIP + Refactored new secret creation on gorup
    
    * Finished with backend functionalities

--- KeyVault/KeyVault/Program.cs
+++ KeyVault/KeyVault/Program.cs
@@ -1,24 +1,41 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using KeyVault.Data;
+using System.Text;
 using KeyVault.DbContext;
 using KeyVault.Services.Groups;
 using KeyVault.Services.Keys;
 using KeyVault.Services.Secrets;
 using KeyVault.Services.Users;
-using Slapper;
+using KeyVault.Tools;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddTransient<KeyVaultContext>(_ => new KeyVaultContext(builder.Configuration));
 builder.Services.AddScoped<IUserServices, UserServices>();
 builder.Services.AddScoped<ISecretsService, SecretsService>();
 builder.Services.AddScoped<IKeysService, KeysService>();
 builder.Services.AddScoped<IGroupsService, GroupsService>();
+builder.Services.AddScoped<CryptoTool>();
+builder.Services.AddScoped<TokenTools>();
+builder.Services.AddScoped<LocalStorage>();
+builder.Services.AddHttpClient();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+            ValidAudience = builder.Configuration["JWT:ValidAudience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+        };
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
